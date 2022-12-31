1db9fd3aeb8055fa750019f46f4976f66b835b0d
AnnOselkova <90327226+AnnOselkova@users.noreply.github.com>
2022-12-31 14:27:22 +0300

    2

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-
 public class Lab1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //массив из 3-х точек и координат
         Point3d[] points = new Point3d[3];
         double[] coords = new double[3];
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //считываем координаты точек
         for(int i = 0; i < 3; i++) {
-            //считывания координат три раза и запись их в массив ((сначала координаты, потом точки)
+            //считывание координат и запись их в массив
             for(int k = 0; k < 3; k++){
                 char coord;
                 if(k == 0)coord = 'x';
@@ -17,6 +16,7 @@ public class Lab1 {
             }
             points[i] = new Point3d(coords[0], coords[1], coords[2]);
         }
+        // проверяем на совпаление
         if(points[0].compare(points[1]) || points[0].compare(points[2]) || points[1].compare(points[2])) {
             System.out.println("Точки совпадают!");
             return;
@@ -24,7 +24,7 @@ public class Lab1 {
         System.out.println("Площадь треугольника: " + computeArea(points[0], points[1], points[2]));
     }
 
-    //Функция для вычисления площади треугольника.
+    // вычисление площади треугольника
     public static double computeArea(Point3d point1, Point3d point2, Point3d point3){
         double a = point1.distanceTo(point2);
         double b = point1.distanceTo(point3);
--- src/Point3d.java
+++ src/Point3d.java
@@ -1,28 +1,27 @@
-public class Point3d extends Point2d{
-    private double xCoord;
-    private double yCoord;
+public class Point3d extends Point2d{ //объявление координат
     private double zCoord;
-    public Point3d(double x, double y, double z){
-       this. xCoord=x;
-       this.yCoord=y;
+    public Point3d(double x, double y, double z){ // конструктор класса
+       super.setX(x);
+       super.setY(y);
        this.zCoord=z;
     }
     public Point3d(){ // базовый конструктор
+
         this(0.0,0.0,0.0);
     }
     public double getZ(){
         return zCoord;
     }
-    public void setZ(double val){
-        this.zCoord=val;
+    public void setZ(double z){
+        this.zCoord=z;
     }
-    //Сравнивает объект с другим объектом.
+    //Сравниваем объект с другим объектом.
     public boolean compare(Point3d point){
-        return (this.xCoord == point.getX() && this.yCoord == point.getY() && this.zCoord == point.getZ());
+        return (this.getX() == point.getX() && this.getY() == point.getY() && this.zCoord == point.getZ());
     }
-    //Возвращает геометрическое расстояние между двумя точками.
+    //Возвращаем расстояние между двумя точками.
     public double distanceTo(Point3d point){
-        return Math.sqrt(Math.pow(this.xCoord - point.getX(), 2) + Math.pow(this.yCoord - point.getY(), 2) + Math.pow(this.zCoord - point.getZ(), 2));
+        return Math.sqrt(Math.pow(this.getX() - point.getX(), 2) + Math.pow(this.getY() - point.getY(), 2) + Math.pow(this.zCoord - point.getZ(), 2));
     }
 
 }
